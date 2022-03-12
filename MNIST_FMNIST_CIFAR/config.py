0dd15eaf9de83d531090d8d2d4013ffa657895e7
Subash Khanal <subash.khanal33@gmail.com>
2022-03-11 22:48:39 -0500

    update config

--- MNIST_FMNIST_CIFAR/config.py
+++ MNIST_FMNIST_CIFAR/config.py
@@ -3,7 +3,7 @@ from easydict import EasyDict as edict
 import os
 cfg = edict()
 
-num_epochs_basemodel = 2#10
+num_epochs_basemodel = 10
 lr_basemodel = 0.0001 
 
 data_path = '/home/skh259/LinLab/LinLab/CAT-XPLAIN/MNIST_FMNIST_CIFAR/checkpoints/'
@@ -20,13 +20,13 @@ device = torch.device('cuda:'+str(gpus) if torch.cuda.is_available() else 'cpu')
 N = 4
 # 0: define global vars(tau, k, etc)
 tau = 0.1
-num_epochs = 2#10
+num_epochs = 10
 
 train_further = True
-post_num_epochs = 2#5
+post_num_epochs = 5
 
 lr = 0.0001
 best_val_acc = 0
-num_init = 1#3 #number of initializations of the explainer
+num_init = 3 #number of initializations of the explainer
 
 tuning = False # Default: False. Wether to stop once the black box model is trained or not
