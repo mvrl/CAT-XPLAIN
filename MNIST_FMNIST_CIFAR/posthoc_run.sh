a748cf1c00d0efe97305f1e8bddae99ab4d9e524
Subash Khanal <subash.khanal33@gmail.com>
2022-06-27 16:51:58 -0400

    posthoc_run.sh fixed

--- MNIST_FMNIST_CIFAR/posthoc_run.sh
+++ MNIST_FMNIST_CIFAR/posthoc_run.sh
@@ -33,16 +33,16 @@ echo "Experiments for FMNIST DONE!!!!"
 
 
 echo "MNIST"
-python ./MNIST_FMNIST_CIFAR/post_hoc.py --num_patches "0.05" --validation "with_test"  --dataset_name "mnist" --dataset_class "partial" --depth 6 --dim 512 --load_bb_model "true" 
+python ./MNIST_FMNIST_CIFAR/post_hoc.py --num_patches "0.05" --validation "with_test"  --dataset_name "mnist" --dataset_class "partial" --depth 6 --dim 512 --load_bb_model "false" 
 echo " --num_patches 0.05"
 
-python ./MNIST_FMNIST_CIFAR/post_hoc.py --num_patches "0.10" --validation "with_test"  --dataset_name "mnist" --dataset_class "partial" --depth 6 --dim 512 --load_bb_model "true" 
+python ./MNIST_FMNIST_CIFAR/post_hoc.py --num_patches "0.10" --validation "with_test"  --dataset_name "mnist" --dataset_class "partial" --depth 6 --dim 512 --load_bb_model "false" 
 echo " --num_patches 0.10"
 
-python ./MNIST_FMNIST_CIFAR/post_hoc.py --num_patches "0.25" --validation "with_test"  --dataset_name "mnist" --dataset_class "partial" --depth 6 --dim 512 --load_bb_model "true" 
+python ./MNIST_FMNIST_CIFAR/post_hoc.py --num_patches "0.25" --validation "with_test"  --dataset_name "mnist" --dataset_class "partial" --depth 6 --dim 512 --load_bb_model "false" 
 echo " --num_patches 0.25"
 
-python ./MNIST_FMNIST_CIFAR/post_hoc.py --num_patches "0.50" --validation "with_test"  --dataset_name "mnist" --dataset_class "partial" --depth 6 --dim 512 --load_bb_model "true" 
+python ./MNIST_FMNIST_CIFAR/post_hoc.py --num_patches "0.50" --validation "with_test"  --dataset_name "mnist" --dataset_class "partial" --depth 6 --dim 512 --load_bb_model "false" 
 echo " --num_patches 0.50"
 
 echo "Experiments for MNIST DONE!!!!"
